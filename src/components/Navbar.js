d227de3fdd5fdfb8c35407119f7172914479bbb8
shagors <shajjadshagor@gmail.com>
2022-05-22 06:56:00 +0600

    signup options add and check in firebase user login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, NavLink } from 'react-router-dom';
+import auth from '../firebase.init';
 
 const Navbar = ({children}) => {
+    const [user] = useAuthState(auth);
     return (
         <div className="drawer drawer-end">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
@@ -12,7 +16,7 @@ const Navbar = ({children}) => {
                         <label for="my-drawer-3" className="btn btn-square btn-ghost">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </label>
-                    </div> 
+                    </div>
                     <div className="flex-none hidden lg:block">
                         <ul className="menu menu-horizontal gap-x-4">
                             <NavLink className='rounded-lg hover:bg-primary hover:text-white p-3' to='/home'>Home</NavLink>
@@ -20,7 +24,7 @@ const Navbar = ({children}) => {
                             <NavLink className='rounded-lg hover:bg-primary hover:text-white p-3' to='/Dashboard'>Dashboard</NavLink>
                             <NavLink className='rounded-lg hover:bg-primary hover:text-white p-3' to='/blogs'>Blogs</NavLink>
                             <NavLink className='rounded-lg hover:bg-primary hover:text-white p-3' to='/portfolio'>Portfolio</NavLink>
-                            <NavLink className='rounded-lg hover:bg-primary hover:text-white p-3' to='/login'>Login</NavLink>
+                            
                         </ul>
                     </div>
                 </div>
