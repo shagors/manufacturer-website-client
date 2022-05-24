99becf70964835a2b373b065f741906fe09de909
shagors <shajjadshagor@gmail.com>
2022-05-24 22:35:35 +0600

    card error handle and card data check and card number check

--- src/pages/Dashboard/CheckoutForm.js
+++ src/pages/Dashboard/CheckoutForm.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { async } from '@firebase/util';
 
 const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [transactionId, setTransactionId] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        if(!stripe || !elements){
+            return;
+        }
+
+    const card = elements.getElement(CardElement);
+    if(card === null){
+        return;
+    }
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        setCardError(error?.message || '');
+        setSuccess('');
+        setProcessing(true);
+
+
+
+}
+
     return (
-        <div>
+        <>
+            <form onSubmit={handleSubmit}>
+                    <CardElement
+                        options={{
+                            style: {
+                                base: {
+                                    fontSize: '16px',
+                                    color: '#424770',
+                                    '::placeholder': {
+                                        color: '#aab7c4',
+                                    },
+                                },
+                                invalid: {
+                                    color: '#9e2146',
+                                },
+                            },
+                        }}
+                    />
+                    <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+                        Pay
+                    </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
             
-        </div>
+        </>
     );
 };
 
